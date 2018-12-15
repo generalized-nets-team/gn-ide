c9a1a765c1694029cb976a023395271da8dc364b
mitex <@>
2018-12-15 19:37:28 +0200

    fix run.sh so it can be called from a different directory

--- deployment/run.sh
+++ deployment/run.sh
@@ -5,5 +5,5 @@ if [ ! -z "`uname -a | grep x86_64`" ]; then
 	SWT=swt-linux64.jar
 fi
 
-java -cp Genedit.jar:$SWT net.generalised.genedit.main.Main "$@"
+java -cp `dirname "$0"`/Genedit.jar:`dirname "$0"`/$SWT net.generalised.genedit.main.Main "$@"
 
